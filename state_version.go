e059050f7dc8c52f13139e515e497c5e4cbc8a53
Artem Vang <vangogius@gmail.com>
2021-06-09 13:42:35 +0300

    SCALRCORE-18479 > Add tests, extend structs for state version

--- state_version.go
+++ state_version.go
@@ -13,23 +13,41 @@ var _ StateVersions = (*state_versions)(nil)
 
 // StateVersions describes all the state versions related methods that the Scalr API supports.
 type StateVersions interface {
+	Create(ctx context.Context, options StateVersionCreateOptions) (*StateVersion, error)
 	ReadByID(ctx context.Context, stateVersionID string) (*StateVersion, error)
 	ReadCurrentFromWorkspace(ctx context.Context, workspaceID string) (*StateVersion, error)
 }
 
 type StateVersion struct {
-	ID        string      `jsonapi:"primary,state-versions"`
-	CreatedAt time.Time   `jsonapi:"attr,created-at,iso8601"`
-	Force     bool        `jsonapi:"attr,force"`
-	Resources []*Resource `jsonapi:"attr,resources"`
+	ID        string                 `jsonapi:"primary,state-versions"`
+	Force     bool                   `jsonapi:"attr,force"`
+	Lineage   string                 `jsonapi:"attr,lineage"`
+	MD5       *string                `jsonapi:"attr,md5"`
+	CreatedAt time.Time              `jsonapi:"attr,created-at,iso8601"`
+	Serial    uint64                 `jsonapi:"attr,serial"`
+	Size      uint64                 `jsonapi:"attr,size"`
+	State     *string                `jsonapi:"attr,state"`
+	Resources []*Resource            `jsonapi:"attr,resources"`
+	Outputs   []*Output              `jsonapi:"attr,outputs"`
+	Modules   map[string]interface{} `jsonapi:"attr,modules"`
+	Providers map[string]interface{} `jsonapi:"attr,providers"`
 
-	// Relations
-	Run       *Run       `jsonapi:"relation,run"`
-	Workspace *Workspace `jsonapi:"relation,workspace"`
+	Workspace            *Workspace    `jsonapi:"relation,workspace"`
+	Run                  *Run          `jsonapi:"relation,run"`
+	NextStateVErsion     *StateVersion `jsonapi:"relation,next-state-version"`
+	PreviousStateVersion *StateVersion `jsonapi:"relation,previous-state-version"`
+}
+
+type Output struct {
+	Name      string  `jsonapi:"attr,name"`
+	Sensitive bool    `jsonapi:"attr,sensitive"`
+	Value     *string `jsonapi:"attr,value"`
 }
 
 type Resource struct {
-	Type string `jsonapi:"attr,type"`
+	Type    string  `jsonapi:"attr,type"`
+	Address string  `jsonapi:"attr,address"`
+	Module  *string `jsonapi:"attr,module"`
 }
 
 // state_versions implements StateVersion.
@@ -37,7 +55,44 @@ type state_versions struct {
 	client *Client
 }
 
-// Read a state version by its ID.
+type StateVersionCreateOptions struct {
+	ID        string                 `jsonapi:"primary,state-versions"`
+	Force     bool                   `jsonapi:"attr,force"`
+	Lineage   string                 `jsonapi:"attr,lineage"`
+	MD5       string                 `jsonapi:"attr,md5"`
+	Serial    uint64                 `jsonapi:"attr,serial"`
+	Size      uint64                 `jsonapi:"attr,size"`
+	State     string                 `jsonapi:"attr,state"`
+	Resources []*Resource            `jsonapi:"attr,resources"`
+	Outputs   []*Output              `jsonapi:"attr,outputs"`
+	Modules   map[string]interface{} `jsonapi:"attr,modules"`
+	Providers map[string]interface{} `jsonapi:"attr,providers"`
+
+	Workspace            *Workspace    `jsonapi:"relation,workspace"`
+	Run                  *Run          `jsonapi:"relation,run"`
+	NextStateVErsion     *StateVersion `jsonapi:"relation,next-state-version"`
+	PreviousStateVersion *StateVersion `jsonapi:"relation,previous-state-version"`
+}
+
+// Read current state version of workspace.
+func (s *state_versions) Create(ctx context.Context, options StateVersionCreateOptions) (*StateVersion, error) {
+	options.ID = ""
+
+	req, err := s.client.newRequest("POST", "state-versions", &options)
+	if err != nil {
+		return nil, err
+	}
+
+	sv := &StateVersion{}
+	err = s.client.do(ctx, req, sv)
+	if err != nil {
+		return nil, err
+	}
+
+	return sv, nil
+}
+
+// Read current state version of workspace.
 func (s *state_versions) ReadByID(ctx context.Context, stateVersionID string) (*StateVersion, error) {
 	if !validStringID(&stateVersionID) {
 		return nil, errors.New("invalid value for state version")
