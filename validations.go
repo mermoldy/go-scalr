7e94d2f897a618ee21581ddca20032c049bcc59a
Artem Vang <vangogius@gmail.com>
2022-03-21 11:42:35 +0200

    SCALRCORE-20549 > Fix merge conflict, remove unused function

--- validations.go
+++ validations.go
@@ -1,7 +1,6 @@
 package scalr
 
 import (
-	"net"
 	"regexp"
 	"strings"
 )
@@ -18,21 +17,4 @@ func validString(v *string) bool {
 // contains a typical string identifier.
 func validStringID(v *string) bool {
 	return v != nil && reStringID.MatchString(*v)
-}
-
-func validIPv4Network(v *string) bool {
-	if v == nil {
-		return false
-	}
-
-	if ip := net.ParseIP(*v); ip != nil && ip.To4() != nil {
-		return true
-	}
-
-	addr, _, err := net.ParseCIDR(*v)
-	if err != nil {
-		return false
-	}
-
-	return addr.To4() != nil
 }
